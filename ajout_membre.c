550e66851228cdb1dfa488c8a3c99207c540c6c6
AzizBenTekaya <aziz.bentekaya@esprit.tn>
2025-10-21 19:20:39 +0100

    création membre.c et codage de la fct ajouter_membre(membre m)

---
+++ ajout_membre.c
@@ -0,0 +1,7 @@
+#include "ajout_membre.h"
+
+int ajouter_membre(membre m){
+FILE *f=fopen("membre.txt","a");
+if(f!=NULL){
+fprintf(f,"id=%s nom=%s prenom=%s jour=%d mois=%sannee=%d type_abo=%s poids=%f masse_muscu=%fmasse_graisseuse=%fsexe=%s taille=%d programme=%s objectif=%s descr_maladie=%s",m.id,m.nom,m.prenom,m.jour,m.mois,m.annee,m.type_abonnement,m.poids,m.masse_musculaire,m.masse_graisseuse,m.sexe,m.taille,m.programme,m.objectif,m.descr_maladie);
+fclose(f);
