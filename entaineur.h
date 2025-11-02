fc6897577b4d4e1ad4e01516f55fd1b49714b203
Meriem0014 <meriem.gadrib@esprit.tn>
2025-11-02 13:49:54 +0100

    entaineur.h

--- entaineur.h
+++ entaineur.h
@@ -1,23 +1,23 @@
 #ifndef ENTRAINEUR_H_INCLUDED
 #define ENTRAINEUR_H_INCLUDED
-#include <gtk/gtk.h>
-typedef struct{
-int id;
-char nom[30],prenom[30];
-int num;
-char email[50],mdp[30];
-int age,capacite;
-char sexe[30],specialite[100],cour[100],nc[100],centre[50];
-int jour;
-int annee;
-int moins,heure;
-}entrianeur;
-typedef struct{
-int id,heure,capacite,jour,moins,annee;
-char nom[50],prenom[50],cour[50],public_cible[100],sexe[50],equipement[50];
-char restriction medicale[50],objectif[50],intensite[50];
-void ajouter_entraineur(entraineur E);
-void supprimer_entraineur(int id);
-void modifier_entraineur(entaineur E);
-void afficher_entraineur();
-void recherche_entraineur(int id);
+#include <stdio.h>
+typedef struct {
+    int id;
+    char nom[30];
+    char prenom[30];
+    char specialite[30];
+    float salaire;
+    char sexe[10];
+    char date_inscription[15];
+    char email[50];
+    char num_tel[20];
+    char cours_privee[5];
+    char centre[30];
+} entraineur;
+
+int ajouter(char *filename, entraineur e);
+int modifier(char *filename, int id, entraineur nouv);
+int supprimer(char *filename, int id);
+entraineur chercher(char *filename, int id);
+
+#endif
