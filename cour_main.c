48dbbf57944cec98b54bdf082ad4420e11358c9d
hiba b <benyoussef.hiba@esprit.tn>
2025-11-06 13:10:46 +0100

    Update cour_main.c

--- cour_main.c
+++ cour_main.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include "cour.h"
 
+
 int main()
 {
     cour c1 = {1, 10, 20, "Yoga", "Collectif", "Lundi", "Amina", "Adultes", "Moyenne", "Aucune", "Souplesse", "Tapis"};
     cour c2 = {2, 14, 15, "Muscu", "Individuel", "Mardi", "Ali", "Hommes", "Forte", "Cœur", "Force", "Haltères"};
     cour c3;
+int choix 
+    do
+        { do {
+        }while(choix<0 || choix >6);
+         switch(choix){
+             case 1:
 
     int x = ajouter("cours.txt", c1);
     if (x) printf("\nAjout réussi"); else printf("\nErreur ajout");
-
+           case 2:
     x = modifier("cours.txt", 1, c2);
     if (x) printf("\nModification réussie"); else printf("\nErreur modification");
-
+             case 3:
     x = supprimer("cours.txt", 2);
     if (x) printf("\nSuppression réussie"); else printf("\nErreur suppression");
-
+             case 4 :
     c3 = chercher("cours.txt", 1);
     if (c3.id != -1)
         printf("\nCours trouvé : %s (%s - %s)", c3.nom_c, c3.type_c, c3.jour_c);
     else
         printf("\nCours introuvable");
-
+             case 5:
     sinscrire("cours.txt", 1, "M001");
     reserver("cours.txt", 1, "C002");
 
     printf("\nInscription et réservation enregistrées.\n");
+         }
     return 0;
 }
