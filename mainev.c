df59f0ecf03f5da721373189a73b74de18050eac
rawenaouina <rawen.aouina2004@gmail.com>
2025-11-06 11:07:14 +0100

    Update and rename main.c to mainev.c

--- mainev.c
+++ mainev.c
@@ -3,39 +3,39 @@
 #include "membre.h"
 int main()
 {
-    // --- TEST ÉVÉNEMENTS ---
+    // --- TEST Ã‰VÃ‰NEMENTS ---
     event e1 = {1, "YogaMatinal", "Sport", 10, 11, 2025, 7, 30, 8, 30, 12.00, "SalleZen"};
     event e2 = {2, "ZumbaParty", "CoursCollectif", 12, 11, 2025, 18, 0, 19, 0, 8.00, "SalleDance"};
 
     ajouter_event("events.txt", e1);
     ajouter_event("events.txt", e2);
 
-    printf("Deux événements ajoutés.\n");
+    printf("Deux Ã©vÃ©nements ajoutÃ©s.\n");
 
 
     int x = ajouter_event("events.txt", e1);
     if (x == 1)
-        printf("Ajout avec succès\n");
+        printf("Ajout avec succÃ¨s\n");
     else
-        printf("Échec ajout\n");
+        printf("Ã‰chec ajout\n");
 
     x = modifier_event("events.txt", 1, e2);
     if (x == 1)
-        printf("Modification avec succès\n");
+        printf("Modification avec succÃ¨s\n");
     else
-        printf("Échec modification\n");
+        printf("Ã‰chec modification\n");
 
     x = supprimer_event("events.txt", 1);
     if (x == 1)
-        printf("Suppression avec succès\n");
+        printf("Suppression avec succÃ¨s\n");
     else
-        printf("Échec suppression\n");
+        printf("Ã‰chec suppression\n");
 
     e3 = chercher_event("events.txt", 1);
     if (e3.id == -1)
-        printf("Événement introuvable\n");
+        printf("Ã‰vÃ©nement introuvable\n");
     else
-        printf("Événement trouvé : %s - %s\n", e3.nom, e3.type);
+        printf("Ã‰vÃ©nement trouvÃ© : %s - %s\n", e3.nom, e3.type);
 
     return 0;
     // --- TEST PARTICIPATIONS ---
@@ -45,21 +45,21 @@ int main()
     ajouter_participation("participations.txt", p1);
     ajouter_participation("participations.txt", p2);
 
-    printf("Participations ajoutées.\n");
+    printf("Participations ajoutÃ©es.\n");
 
     // --- RECHERCHE ---
     participation p3 = chercher_participation("participations.txt", 101, 1);
     if (p3.id_membre != -1)
-        printf("Participation trouvée : %s %s à %s (%s)\n", p3.nom, p3.prenom, p3.nom_event, p3.salle);
+        printf("Participation trouvÃ©e : %s %s Ã  %s (%s)\n", p3.nom, p3.prenom, p3.nom_event, p3.salle);
     else
         printf("Participation introuvable.\n");
 
     // --- SUPPRESSION ---
     int x = supprimer_participation("participations.txt", 102, 2);
     if (x == 1)
-        printf("Participation supprimée.\n");
+        printf("Participation supprimÃ©e.\n");
     else
-        printf("Aucune participation supprimée.\n");
+        printf("Aucune participation supprimÃ©e.\n");
 
     return 0;
 }
